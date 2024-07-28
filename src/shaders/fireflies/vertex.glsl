18ee11ff4f11eefdd5dd2112720a173fc2e34b4c
alessandrobattiato <alessandroDev@outlook.it>
2024-07-28 19:51:07 +0200

    fix: particles size resize accordingly to camera distance

--- src/shaders/fireflies/vertex.glsl
+++ src/shaders/fireflies/vertex.glsl
@@ -6,5 +6,7 @@ void main() {
     vec4 projectionPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectionPosition;
+    
     gl_PointSize = 40.0 * uPixelRatio; // this fixes the different devices' pixel density so 40 px is going to be 40 for all of the resolutions
+    gl_PointSize *= (1.0 / - viewPosition.z); // particles now resize accordingly to camera distance
 }
