3294e11ad8ca80ebfbad9117c8cd76b89c23e441
alessandrobattiato <alessandroDev@outlook.it>
2024-07-28 20:38:44 +0200

    feat: add custom color gradient

--- src/shaders/portal/fragment.glsl
+++ src/shaders/portal/fragment.glsl
@@ -1,5 +1,7 @@
+varying vec2 vUv;
+
 void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    gl_FragColor = vec4(vUv, 1.0, 1.0);
 
     #include <colorspace_fragment>
 }
--- src/shaders/portal/vertex.glsl
+++ src/shaders/portal/vertex.glsl
@@ -1,3 +1,5 @@
+varying vec2 vUv; // we send this variable to the fragment using the varying and we already have the position, uv and other variables since we are dealing with a ShaderMaterial and not a RawShaderMaterial
+
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
@@ -5,4 +7,6 @@ void main() {
 
     // gl_Pointsize is not needed since we are not dealing with points but a classic geometry
     gl_Position = projectionPosition;
+
+    vUv = uv; // as stated above, we already have the uv
 }
